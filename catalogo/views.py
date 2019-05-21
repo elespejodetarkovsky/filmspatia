8269047cd734038d42569b5e2ad619cff419dd20
sebas <sebas@localhost>
2019-05-21 23:30:07 +0200

    se han colocado las primeras plantillas. Verificar datos

--- catalogo/views.py
+++ catalogo/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+#La función render genera html en funcion de datos y plantillas
+
+from .models import Film, Director, FilmInstance, Genero
+
+def index(request):
+    """
+    Función vista para la página inicio del sitio.
+    """
+    # Genera contadores de algunos de los objetos principales
+    num_films=Film.objects.all().count()
+    num_instances=FilmInstance.objects.all().count()
+    # Libros disponibles (status = 'a')
+    #num_instances_available=BookInstance.objects.filter(status__exact='a').count()
+    num_directores = Director.objects.count()  # El 'all()' esta implícito por defecto.
+    
+    # Renderiza la plantilla HTML index.html con los datos en la variable contexto
+    return render(
+        request,
+        'index.html',
+        context={'num_films':num_films,'num_instances':num_instances,'num_directores':num_directores},
+    )
